a79172abd629fd6167ba41d26ef5c44ef4ce2bd5
Andrew <AndrewSilver@mail.ru>
2024-03-14 14:48:27 +0300

    изменили

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,10 @@
 ﻿System.Console.WriteLine("Hi");
+
+int value = 100;
+double d = 5.5;
+char ch = 'g';
+string st = "mmm";
+System.Console.WriteLine(value);
+System.Console.WriteLine(d);
+System.Console.WriteLine(ch);
+System.Console.WriteLine(st);
